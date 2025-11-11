9c037cbfc42a4fc5921ecb3e60e467c0ee343d73
Muhammad Faisal <mflqp9@gmail.com>
2025-11-11 15:22:56 +0500

    dist add

---
+++ dist/routes/routes.js
@@ -0,0 +1,5 @@
+import express from "express";
+import { get } from "../controllers/controllers.js";
+const router = express.Router();
+router.get("/", get);
+export default router;
---
+++ dist/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import config from "./config/environment.js";
+import { dbConnected } from "./config/database.js";
+import router from "./routes/routes.js";
+const app = express();
+app.use(express.json());
+app.use(morgan("combined"));
+app.use(cors());
+dbConnected(); //Connect to your mongodb database if !node_env="development"
+app.use("/", router);
+const node_env = config.NODE_ENV;
+if (node_env == "development") {
+    app.listen(config.PORT, config.HOST, () => {
+        console.log(`server is running on http://${config.HOST}:${config.PORT}`);
+    });
+}
