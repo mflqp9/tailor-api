edb1f61a2704ac036910fc7bc964899ea084adcd
Muhammad Faisal <mflqp9@gmail.com>
2025-12-25 18:06:37 +0500

    late

--- src/controllers/customer.ts
+++ src/controllers/customer.ts
@@ -6,29 +6,23 @@ import { AuthRequest } from "../middleware/userAuth.js";
 
 export const getCustomersDetail = async (req: AuthRequest, res: Response) => {
   try {
-    const role = req.user!.role.toLowerCase();
-    const userBranchId = req.user!.branchId;
-    const branches = req.user!.branches;
+    const branchId = req.user?.branchId;
+
+    if (!branchId) {
+      return res.status(401).json({
+        succeed: false,
+        message: "Branch not found",
+      });
+    }
 
     const search = (req.query.search as string) || "";
     const page = Number(req.query.page || 1);
     const limit = Number(req.query.limit || 10);
     const skip = (page - 1) * limit;
 
-    let query: any = {};
-
-    if (role === "user") {
-      query.branchid = userBranchId;
-    } else if (role === "admin") {
-      if (!branches?.length) {
-        return res.status(403).json({
-          succeed: false,
-          message: "No branches assigned to admin",
-        });
-      }
-      query.branchid = { $in: branches };
-    }
-    // super_admin → no branch restriction
+    let query: any = {
+      branchid: branchId,
+    };
 
     if (search) {
       query.$or = [
@@ -36,12 +30,6 @@ export const getCustomersDetail = async (req: AuthRequest, res: Response) => {
         { mobile: { $regex: search, $options: "i" } },
       ];
     }
-
-    const CACHE_VERSION = "v2";
-    const cacheKey = `customers:${CACHE_VERSION}:${role}:${page}:${limit}:${JSON.stringify(query)}`;
-
-    const cached = await redis.get(cacheKey);
-    if (cached) return res.json(JSON.parse(cached));
 
     const [data, total] = await Promise.all([
       CustomerModel.find(query)
@@ -51,27 +39,14 @@ export const getCustomersDetail = async (req: AuthRequest, res: Response) => {
 
       CustomerModel.countDocuments(query),
     ]);
-
-    const response = {
+    res.json({
       succeed: true,
-      message: "Customers fetched successfully",
-      page,
-      limit,
-      total,
-      totalPages: Math.ceil(total / limit),
       data,
-      error: null,
-    };
-
-    await redis.setex(cacheKey, 30, JSON.stringify(response));
-    res.json(response);
-  } catch (err) {
-    console.error(err);
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      succeed: false,
-      message: "Server error",
-      error: err,
+      totalPages: Math.ceil(total / limit),
     });
+  } catch (error) {
+    res.status(500).json({ succeed: false,error:error });
   }
 };
+
 
